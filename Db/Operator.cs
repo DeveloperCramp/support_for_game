430a03e96e0ade4550fef1f1390c94b3e8476ddd
DeveloperCramp <biodubstep@gmail.com>
2022-05-23 23:22:16 +0300

    Add Operator class

---
+++ Db/Operator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApp.Models
+{
+    public class Operator
+    {
+        public Guid Id { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
