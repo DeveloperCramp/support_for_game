958840fc453c50d5fdb3a3ffdc83f78313bcf764
DeveloperCramp <biodubstep@gmail.com>
2022-05-23 23:22:48 +0300

    Add Context for Db

---
+++ Db/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WebApp.Models;
+
+namespace Db
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Operator> Operators { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;user=root;password=MeGamePass15;database=megame_support;", 
+                new MySqlServerVersion(new Version(8, 0, 29))
+                );
+        }
+
+    }
+}
