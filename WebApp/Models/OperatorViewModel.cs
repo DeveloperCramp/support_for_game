faea18f18ae6b9d87e993056a82e226dd8c7e1a1
DeveloperCramp <biodubstep@gmail.com>
2022-05-23 22:43:13 +0300

    Add Operator model

---
+++ WebApp/Models/OperatorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models
+{
+    public class OperatorViewModel
+    {
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
