892872c7bcbe341b5c16f0e2b1bfcea321b559af
DeveloperCramp <biodubstep@gmail.com>
2022-05-23 22:39:15 +0300

    Add Controller and View for Authorization

---
+++ WebApp/Controllers/AuthorizationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
