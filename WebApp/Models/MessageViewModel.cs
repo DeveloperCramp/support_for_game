ac92bef70b12fc82bf0b82c245c350848c789ae5
DeveloperCramp <biodubstep@gmail.com>
2022-05-23 22:43:04 +0300

    Add Message model

---
+++ WebApp/Models/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApp.Models
+{
+    public class MessageViewModel
+    {
+        public PlayerViewModel Player { get; set; }
+        public OperatorViewModel Operator { get; set; }
+        public DateTime CreationTime { get; set; }
+        public bool IsRead { get; set; }
+        public string Content { get; set; }
+    }
+}
