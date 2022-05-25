6455a276538f570ece3e813d24fd0ac8c643dd80
DeveloperCramp <biodubstep@gmail.com>
2022-05-25 17:48:04 +0300

    Add Guid Id

--- WebApp/Models/PlayerViewModel.cs
+++ WebApp/Models/PlayerViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace WebApp.Models
+﻿using System;
+
+namespace WebApp.Models
 {
     public class PlayerViewModel
     {
+        public Guid Id { get; set; }
         public string Nickname { get; set; }
         public string Token { get; set; }
     }
