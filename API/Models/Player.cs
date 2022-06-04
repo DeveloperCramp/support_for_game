123da06dc18e7fdf840d3f958bfa6fe953eddb9a
DeveloperCramp <biodubstep@gmail.com>
2022-06-04 17:04:14 +0300

    Add DataAnnotations for Nickname and Token

--- API/Models/Player.cs
+++ API/Models/Player.cs
@@ -1,8 +1,15 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
 {
     public class Player
     {
+        [Required(ErrorMessage = "Заполните поле")]
+        [StringLength(16, MinimumLength = 3, ErrorMessage = "Nickname не может быть меньше 3 символов и максимум 16 символов")]
         public string Nickname { get; set; }
+
+        [Required(ErrorMessage = "Токен не может быть пустым")]
+        [StringLength(40, MinimumLength = 40, ErrorMessage = "Токен не валидный")]
         public string Token { get; set; }
     }
 }
