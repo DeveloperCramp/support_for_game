6236e59e62c5f5350507dd400090e15bef584a82
DeveloperCramp <biodubstep@gmail.com>
2022-06-06 19:14:34 +0300

    Add Scripts

--- Unity_Client/Assets/Scripts/Send_Message.cs
+++ Unity_Client/Assets/Scripts/Send_Message.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using Microsoft.AspNetCore.SignalR.Client;
+using Assets.Model;
 
 public class Send_Message : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text ReceivedText;
+    public InputField MessageInput;
+    private SignalRConnector connector;
+    public async Task Start()
     {
-        
-    }
+        connector = new SignalRConnector();
+        connector.OnMessageReceived += UpdateReceivedMessages;
 
-    // Update is called once per frame
-    void Update()
+        await connector.InitAsync();
+    }
+    private void UpdateReceivedMessages(Message newMessage)
     {
-        
+        var lastMessages = this.ReceivedText.text;
+        if (string.IsNullOrEmpty(lastMessages) == false)
+        {
+            lastMessages += "\n";
+        }
+        lastMessages += $"User:{newMessage.UserName} Message:{newMessage.Text}";
+        this.ReceivedText.text = lastMessages;
+    }
+    private async void SendMessage()
+    {
+        await connector.SendMessageAsync(new Message
+        {
+            UserName = "Example",
+            Text = MessageInput.text,
+        });
     }
 }
