5e1104898a94f224612b5513b52568963a2b903f
DeveloperCramp <biodubstep@gmail.com>
2022-06-06 19:11:50 +0300

    Add WebSocket

---
+++ WebSocket/DialogHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Megame_support.WebSocket
+{
+    public class DialogHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("Send", message);
+        }
+    }
+}
